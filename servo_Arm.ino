f8e042dad85dbb8b7fb24fd442290ecf7f3d84d2
SamisjZ <86025722+SamisjZ@users.noreply.github.com>
2021-06-19 15:59:38 +0300

    Update servo_Arm.ino

--- servo_Arm.ino
+++ servo_Arm.ino
@@ -41,32 +41,32 @@ void setup() {
   
   servo5.attach(10);
   servo5.write(90);
-  delay(2000);           // to stay at initial angle for 2 seconds
+  
 }
 
 void loop() {
   val1 = analogRead(ptpin1);            // reads the value of the potentiometer (value between 0 and 1023)
-  val1 = map(val1, 0, 1023, 15, 165);     // scale it to use it with the servo (angle value between 15 and 165 so you dont stress the servo)
+  val1 = map(val1, 0, 1023, 90, 180);     // scale it to use it with the servo (angle value at 90 and 180 becuse we want its movment in 90 degrees range so any 90 degrees will work)
   servo1.write(val1);                  // sets the servo position according to the scaled value
   delay(10);                           // waits for the servo to get there
 
   val2 = analogRead(ptpin2);
-  val2 = map(val2, 0, 1023, 15, 165);
+  val2 = map(val2, 0, 1023, 90, 180);
   servo2.write(val2);
   delay(10); 
 
   val3 = analogRead(ptpin3);
-  val3 = map(val3, 0, 1023, 15, 165);
+  val3 = map(val3, 0, 1023, 90, 180);
   servo3.write(val3);
   delay(10); 
 
   val4 = analogRead(ptpin4);
-  val4 = map(val4, 0, 1023, 15, 165);
+  val4 = map(val4, 0, 1023, 90, 180);
   servo4.write(val4);
   delay(10); 
 
   val5 = analogRead(ptpin5);
-  val5 = map(val5, 0, 1023, 15, 165);
+  val5 = map(val5, 0, 1023, 90, 180);
   servo5.write(val5);
   delay(10); 
   
