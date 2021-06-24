99403d9a7e86bea65488a3bde9af6edda10a8a0a
SamisjZ <86025722+SamisjZ@users.noreply.github.com>
2021-06-24 04:59:58 +0300

    servo arm 90 degree code

--- servo_Arm_90.ino
+++ servo_Arm_90.ino
@@ -1,22 +1,50 @@
-#include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+
+#include <Servo.h> //include servo library
+
+Servo servo1;  // create servo object to control a servo
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
 
 int pos = 0;    // variable to store the servo position
 
+
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  
+  servo1.attach(3);  // attaches the servo to digital pin
+  
+  servo2.attach(5);
+  
+  servo3.attach(6);
+  
+  servo4.attach(9);
+  
+  servo5.attach(10);
+  
+
 }
 
 void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  for (pos = 0; pos <= 90;pos += 1) { // goes from 0 degrees to 180 degrees, in steps of 1 degree
+                 
+  servo1.write(pos);   // tell servo to go to position in variable 'pos'               
+  servo2.write(pos);
+  servo3.write(pos);
+  servo4.write(pos);
+  servo5.write(pos);
+    delay(10);            // waits for the servo to get there
+
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+  
+  for (pos = 90;pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees, in steps of 1 degree
+                 
+  servo1.write(pos);   // tell servo to go to position in variable 'pos'               
+  servo2.write(pos);
+  servo3.write(pos);
+  servo4.write(pos);
+  servo5.write(pos);
+    delay(10);                        // waits for the servo to get there
   }
 }
